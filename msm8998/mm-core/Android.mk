806a5f3c5499121f4d56c353a369eb300ca5dad0
Brijesh Patel <pbrijesh@codeaurora.org>
2019-12-31 14:13:40 +0530

    mm-core: Generate omx core header library.
    CRs-Fixed: 2596430
    
    Change-Id: If5dab1762e30486193a221d8b604193cb989c3e2

--- msm8998/mm-core/Android.mk
+++ msm8998/mm-core/Android.mk
@@ -67,7 +67,12 @@ endif
 #===============================================================================
 
 LOCAL_C_INCLUDES        := $(LOCAL_PATH)/src/common
-LOCAL_C_INCLUDES        += $(LOCAL_PATH)/inc
+
+LOCAL_HEADER_LIBRARIES := \
+        libomxcore_headers
+
+LOCAL_EXPORT_HEADER_LIBRARY_HEADERS := libomxcore_headers
+
 LOCAL_PRELINK_MODULE    := false
 LOCAL_MODULE            := libOmxCore
 LOCAL_LICENSE_KINDS     := SPDX-license-identifier-BSD SPDX-license-identifier-MIT
@@ -99,7 +104,12 @@ include $(BUILD_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_C_INCLUDES        := $(LOCAL_PATH)/src/common
-LOCAL_C_INCLUDES        += $(LOCAL_PATH)/inc
+
+LOCAL_HEADER_LIBRARIES := \
+        libomxcore_headers
+
+LOCAL_EXPORT_HEADER_LIBRARY_HEADERS := libomxcore_headers
+
 LOCAL_PRELINK_MODULE    := false
 LOCAL_MODULE            := libmm-omxcore
 LOCAL_LICENSE_KINDS     := SPDX-license-identifier-BSD SPDX-license-identifier-MIT
@@ -123,5 +133,13 @@ LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/qc_registry_table.c
 endif
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libomxcore_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc
+LOCAL_PROPRIETARY_MODULE := true
+
+include $(BUILD_HEADER_LIBRARY)
 
 endif #BUILD_TINY_ANDROID
